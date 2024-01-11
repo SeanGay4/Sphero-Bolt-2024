90bf8f6ff7df272759685ac47eb380898abcd94f
SeanGay4 <seangay30@gmail.com>
2024-01-11 08:12:50 -0500

    Hello World connected and stable

--- Sandbox.js
+++ Sandbox.js
@@ -1,3 +1,13 @@
 //Programmer: Sean Gay
 //Date: 1.10.2024
-//Program: Sphero Bolt Testing
+//Program: Sphero Bolt Testing\
+//Bolt ID: 33DF
+
+async function startProgram() {
+	await speak("Hello World", true);
+	setMainLed({ r: 0, g: 0, b: 255});
+	setSpeed(60);
+	await delay (2);
+	setSpeed(0);
+
+}
