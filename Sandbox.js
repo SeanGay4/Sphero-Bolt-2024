fdf3d0b31e1986ca26edd24fd08b3bf9653505f6
SeanGay4 <seangay30@gmail.com>
2024-01-19 08:26:52 -0500

    figure 8 tested and stable

--- Sandbox.js
+++ Sandbox.js
@@ -4,26 +4,26 @@
 //Bolt ID: 33DF
 /*
 async function startProgram() {
-	await speak("Hello World", true);
-	setMainLed({ r: 0, g: 0, b: 255});
-	setSpeed(60);
-	await delay (2);
-	setSpeed(0);
+await speak("Hello World", true);
+setMainLed({ r: 0, g: 0, b: 255});
+setSpeed(60);
+await delay (2);
+setSpeed(0);
 
 }
 */
 /*
 // Square w/ different colors for each line
-	async function startProgram() {
-	setMainLed({r:255,g:0,b:0})
-	await roll(90,65,2)
-	setMainLed({r:128,g:0,b:128})
-	await roll(180,65,2)
-	setMainLed({r:137,g:240,b:207})
-	await roll(270,65,2)
-	setMainLed({r:255,g:105,b:180})
-	await roll(360,65,2)
-	}
+async function startProgram() {
+setMainLed({r:255,g:0,b:0})
+await roll(90,65,2)
+setMainLed({r:128,g:0,b:128})
+await roll(180,65,2)
+setMainLed({r:137,g:240,b:207})
+await roll(270,65,2)
+setMainLed({r:255,g:105,b:180})
+await roll(360,65,2)
+}
 */
 
 
@@ -31,26 +31,30 @@ async function startProgram() {
 
 
 // Square w/ different colors and Sounds for each line
-
-async function startProgram() {
-	setMainLed({r:255,g:0,b:0})
-	await R2D2.Sound.General.Burnout.play()
-	await roll(90,65,2)
-	
-	setMainLed({r:128,g:0,b:128})
-	await roll(180,65,2)
-	setMainLed({r:137,g:240,b:207})
-	await roll(270,65,2)
-	setMainLed({r:255,g:105,b:180})
-	await roll(360,65,2)
-	}
-
-
-
-
 /*
-// Figure 8
- setSpeed(2)
- await spin(360)
- await spin(360)
+async function startProgram(){
+
+setMainLed({r:255,g:0,b:0})
+await Sound.EightBit.LevelBegin.play()
+await roll(90,65,2)
+setMainLed({r:128,g:0,b:128})
+await Sound.EightBit.LevelComplete.play()
+await roll(180,65,2)
+setMainLed({r:137,g:240,b:207})
+await Sound.ScienceFiction.LaserRapidFire.play()
+await roll(270,65,2)
+setMainLed({r:255,g:105,b:180})
+await Sound.ScienceFiction.Lightsaber.play()
+await roll(360,65,2)
+
+}
 */
+
+// Figure 8
+async function startProgram(){
+await setSpeed(45) ; await spin(360,4)
+await stopRoll()
+await roll(180,45,2)
+await setSpeed(45) ; await spin(360,4)
+await stopRoll()
+}
