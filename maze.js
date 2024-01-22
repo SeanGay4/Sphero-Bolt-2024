5ef1416f27fbab1f69e7f5d88b0a8ea361976966
SeanGay4 <seangay30@gmail.com>
2024-01-22 08:43:06 -0500

    Blue LED Untested

--- maze.js
+++ maze.js
@@ -4,12 +4,16 @@
 // Program: Maze
 //Bolt ID: 33DF
 
-// Start Message and Stable
 
 async function startProgram(){
 await speak("Start", true);	
-await scrollMatrixText('Start', { r: 66, g: 66, b: 66 }, 75, true)    
-	
+await scrollMatrixText('Start', { r: 66, g: 66, b: 66 }, 75, true)    	
+// Blue LED + Movement
+await roll(360, 130, 2)
+await scrollMatrixText('BLUE LED', { r: 0, g: 0, b: 255 }, 75, true)
+await setMainLed({ r: 0, g: 0, b: 255 })
+
+
 }
 
-// Blue LED
+
