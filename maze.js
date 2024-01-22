9ba923b53cf9612154307474a72284663c5ca43e
SeanGay4 <seangay30@gmail.com>
2024-01-22 08:31:32 -0500

    says and shows start stable

--- maze.js
+++ maze.js
@@ -3,3 +3,14 @@
 // Date: 1.22.2024
 // Program: Maze
 //Bolt ID: 33DF
+
+
+async function startProgram(){
+await speak("Start", true);	
+await scrollMatrixText('Start', { r: 66, g: 66, b: 66 }, 75, true)    
+
+
+	
+	
+	
+}
