b1c65bce1ac84be2d26ed3b60b7a2f3fbcb4bcaf
SeanGay4 <seangay30@gmail.com>
2024-01-25 08:19:03 -0500

    Movement and red LED tested and stable

--- maze.js
+++ maze.js
@@ -6,17 +6,22 @@
 
 
 async function startProgram(){
-await speak("Start", true);	
+await speak("Start!", true);	
 await scrollMatrixText('Start!', { r: 66, g: 66, b: 66 }, 75, true)    	
 // Blue LED + Movement
-await roll(360, 75, 2)
+await roll(360, 75, 2.035)
 await scrollMatrixText('BLUE LED', { r: 0, g: 0, b: 255 }, 75, true) ; await setFrontLed({ r: 0, g: 0, b: 255 }) ; await setBackLed({ r: 0, g: 0, b: 255 }) 
 await setMainLed({ r: 0, g: 0, b: 255 })
 // Move toward Noise and make Noise
-await roll(450,75, 1.705)
+await roll(450,75, 1.605)
 await Sound.EightBit.LevelComplete.play()
 // Movement + RED LED
-await roll(540,75,1.5)
-}
-
-
+await roll(540,75,1.065)
+await stopRoll
+await stopRoll
+await roll(560,35,1.5)
+await stopRoll
+await scrollMatrixText('RED LED', { r: 255, g: 0, b: 0 }, 75, true) ; await setFrontLed({ r: 255, g: 0, b: 0 }) ; await setBackLed({ r: 255, g: 0, b: 0 }) 
+await setMainLed({ r: 255, g: 0, b: 0 })
+await stopRoll
+await roll(475,60,1)
